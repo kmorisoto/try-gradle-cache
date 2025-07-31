fb48d5974f7f07f71dd7010b772728f8f7785795
morisoto <k.morisoto@quo-digital.jp>
2025-07-31 20:22:28 +0900

    commit

---
+++ try-gradle-cache/src/main/kotlin/com/example/trygradlecache/TryGradleCacheApplication.kt
@@ -0,0 +1,11 @@
+package com.example.trygradlecache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TryGradleCacheApplication
+
+fun main(args: Array<String>) {
+    runApplication<TryGradleCacheApplication>(*args)
+}
